2843fe831965fe734247ccfd8eff365cd0ccafdc
Chawintee <Chawinteetea@gmail.com>
2020-04-26 14:13:23 +0700

    add delete with confirm

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -1,10 +1,10 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from "../config/axios";
 
 function App() {
 
-  const [inputText,setInputText] = useState("")
+  const [inputText, setInputText] = useState("")
   const [list, setList] = useState([])
   const [idx, setIdx] = useState(0)
 
@@ -21,15 +21,15 @@ function App() {
     const result = await axios.get("/t");
     setList(result.data);
     console.log(list);
-    
+
   }
 
 
   //use effect
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchData()
-  },[]);
+  }, []);
 
 
 
@@ -37,61 +37,61 @@ function App() {
 
   const addToList = async (e) => {
     // console.log(e.key)
-    if(e.key === "Enter" && inputText !== ""){
+    if (e.key === "Enter" && inputText !== "") {
       // const inputObj = {id: idx,task: inputText};
-            const body = {
-              task: inputText,
-            }
-            await axios.post("/t",body);
+      const body = {
+        task: inputText,
+      }
+      await axios.post("/t", body);
       // setList([...list,inputObj]);
       fetchData();
-      setIdx(idx+1);
+      setIdx(idx + 1);
       console.log(list);
       setInputText("");
     }
   }
 
-  
+
 
   //Edit (UPDATE)
 
-  
-  const onClickToShowInput = async (targetId,editStatus) => {
+
+  const onClickToShowInput = async (targetId, editStatus) => {
     // console.log(targetId)
     // console.log(editStatus)
     const body = {
       edit_status: !editStatus,
     }
-    await axios.put(`/t/${targetId}`,body);
+    await axios.put(`/t/${targetId}`, body);
     fetchData();
   };
 
 
-  const [editText,setEditText] = useState("");
+  const [editText, setEditText] = useState("");
   const inputEditText = (e) => {
     setEditText(e.target.value)
     // console.log(e.target.value)
   }
 
 
-  const changeToEditButton = async (targetId,editStatus) => {
+  const changeToEditButton = async (targetId, editStatus) => {
     const body = {
-      edit_status : !editStatus
+      edit_status: !editStatus
     }
-    await axios.put(`/t/${targetId}`,body);
+    await axios.put(`/t/${targetId}`, body);
     fetchData();
   };
 
-  const onEnterToEdit = async(e,targetId,editStatus) => {
+  const onEnterToEdit = async (e, targetId, editStatus) => {
     // console.log(e.key);
     // console.log(targetId);
     // console.log(editStatus);
-    if(e.key == "Enter" && editText !== ""){
+    if (e.key === "Enter" && editText !== "") {
       const body = {
-        task : editText,
-        edit_status : !editStatus,
+        task: editText,
+        edit_status: !editStatus,
       }
-      await axios.put(`/t/${targetId}`,body);
+      await axios.put(`/t/${targetId}`, body);
       fetchData();
       setEditText("")
     }
@@ -102,11 +102,26 @@ function App() {
   // DELETE (DELETE)
 
   const onDeleteList = async (targetId) => {
-    
+
     // console.log(targetId)
     await axios.delete(`/t/${targetId}`);
     fetchData();
 
+  }
+
+
+  // DELETE WITH CONFIRM (DELETE)
+
+  const onDeleteListWithConfirm = async (targetId) => {
+    if(window.confirm("Would you like to delete this item?")){
+      await axios.delete(`/t/${targetId}`)
+      .then((result)=>{
+        alert(`Delete Task ID: ${targetId}`);
+      })
+      fetchData();
+    }else {
+      console.log('Do nothing')
+    }
   }
 
 
@@ -122,14 +137,15 @@ function App() {
   return (
     <div className="App">
       <ul>
-  {list.map(ele => 
-  <li key={ele.id}>{ele.task}  {ele.post_code} 
-  <img src={ele.profile_picture} style={{width:"20px" , height:"20px" } }/> 
-  <button onClick={()=>onDeleteList(ele.id)} >Del</button>
-  {ele.edit_status ? <input onChange={inputEditText} onDoubleClick={()=>changeToEditButton(ele.id,ele.edit_status)} onKeyPress={(e)=>onEnterToEdit(e,ele.id,ele.edit_status)} value={editText}/> : <button onClick={()=>onClickToShowInput(ele.id,ele.edit_status)} >Edit</button>}
-  </li>) }
+        {list.map(ele =>
+          <li key={ele.id}>{ele.task}  {ele.post_code}
+            <img src={ele.profile_picture} style={{ width: "20px", height: "20px" }} />
+            <button onClick={() => onDeleteList(ele.id)} >Del</button>
+            <button onClick={() => onDeleteListWithConfirm(ele.id)} >Del With Confirm</button>
+            {ele.edit_status ? <input onChange={inputEditText} onDoubleClick={() => changeToEditButton(ele.id, ele.edit_status)} onKeyPress={(e) => onEnterToEdit(e, ele.id, ele.edit_status)} value={editText} /> : <button onClick={() => onClickToShowInput(ele.id, ele.edit_status)} >Edit</button>}
+          </li>)}
       </ul>
-      <input onChange={inputTextFn} value={inputText} onKeyPress={addToList}/>
+      <input onChange={inputTextFn} value={inputText} onKeyPress={addToList} />
       <button>Add</button>
     </div>
   );
